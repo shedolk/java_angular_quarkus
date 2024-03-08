567dacb815d732d53011505c06dd13883e94f5b3
Filipe Dias <filipedias25.fd@gmail.com>
2024-03-07 22:12:17 -0300

    first commit

--- src/main/java/br/unitins/topicos2/autopecas/dto/CidadeDTO.java
+++ src/main/java/br/unitins/topicos2/autopecas/dto/CidadeDTO.java
@@ -3,10 +3,7 @@ package br.unitins.topicos2.autopecas.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record CidadeDTO(
-    @NotBlank(message = "O campo nome deve ser informado.")
-    String nome,
+public record CidadeDTO(@NotBlank(message = "O campo nome deve ser informado.") String nome,
 
-    @NotNull(message = "O campo idEstado deve ser informado.")
-    Long idEstado
-) { }
+        @NotNull(message = "O campo idEstado deve ser informado.") Long idEstado) {
+}
