492166405325fc6b9636215aa8c13b10e6f998f1
Filipe Dias <filipedias25.fd@gmail.com>
2024-03-08 08:12:11 -0300

    Arrumando imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,16 +4,5 @@ insert into estado (nome, sigla) values( 'São Paulo', 'SP');
 insert into estado (nome, sigla) values( 'Rio de Janeiro', 'RJ');
 insert into estado (nome, sigla) values( 'Pará', 'PA');
 
-<<<<<<< HEAD
 insert into usuario (nome, email , idade , senha) values( 'Matheus','Matheus@gmail.com', 18 , 'SDFDSFS');
 insert into usuario (nome, email , idade , senha) values( 'Filipe','Filipe@gmail.com',19 ,'SDFSDFDS');
-=======
-insert into usuario (nome, email) values( 'Matheus','Matheus@gmail.com');
-insert into usuario (nome, email) values( 'Filipe','Filipe@gmail.com');
-
-insert into cidade (nome, id_estado) values( 'Palmas', 1);
-insert into cidade (nome, id_estado) values( 'Paraíso', 1);
-insert into cidade (nome, id_estado) values( 'Gurupi', 1);
-insert into cidade (nome, id_estado) values( 'Goiânia', 2);
-insert into cidade (nome, id_estado) values( 'Anápolis', 2);
->>>>>>> 8e5c24dfc3db3c820c03c623287293980a05b3e4
