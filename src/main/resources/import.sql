d94ab37503cbfb5d7c1f3b1c7fb9ed2488cc50b2
Filipe Dias <filipedias25.fd@gmail.com>
2024-03-10 10:54:11 -0300

    Arrumando models

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,4 @@
-insert into estado (nome, sigla) values( 'Tocantins', 'TO');
-insert into estado (nome, sigla) values( 'Goiás', 'GO');
-insert into estado (nome, sigla) values( 'São Paulo', 'SP');
-insert into estado (nome, sigla) values( 'Rio de Janeiro', 'RJ');
-insert into estado (nome, sigla) values( 'Pará', 'PA');
+
 
 insert into usuario (nome, email , idade , senha) values( 'Matheus','Matheus@gmail.com', 18 , 'SDFDSFS');
 insert into usuario (nome, email , idade , senha) values( 'Filipe','Filipe@gmail.com',19 ,'SDFSDFDS');
